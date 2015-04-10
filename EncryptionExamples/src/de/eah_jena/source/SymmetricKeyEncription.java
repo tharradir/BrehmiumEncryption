18c4c9c783ce59b4407c2b967495d3677d65429f
Tobias Möller <t.moeller95@hotmail.de>
2015-04-10 15:37:19 +0200

    Verbesserungen

--- EncryptionExamples/src/de/eah_jena/source/SymmetricKeyEncription.java
+++ EncryptionExamples/src/de/eah_jena/source/SymmetricKeyEncription.java
@@ -56,44 +56,6 @@ private MessageDigest sha;
 		secretKeySpec = new SecretKeySpec(key, "AES");
 	}
 
-//    public static void main(String[] args) throws Exception {
-//        
-//        // Das Passwort 
-//        String keyStr = "geheim";
-//        // byte-Array erzeugen
-//        byte[] key = (keyStr).getBytes("UTF-8");
-//        // aus dem Array einen Hash-Wert erzeugen mit MD5 oder SHA
-//        MessageDigest sha = MessageDigest.getInstance("MD5");
-//        key = sha.digest(key);
-//        // nur die ersten 128 bit nutzen; da AES auf eine feste Größe beschränkt ist
-//        key = Arrays.copyOf(key, 16); 
-//        // der fertige Schluessel
-//        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
-// 
-//        
-//
-//        //zu verschlüsselnder Text
-//        byte[] byteText = "quelle: http://stackoverflow.com/questions/20796042/aes-encryption-and-decryption-with-java".getBytes();
-//
-//        AesCipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-//        
-//        //Verschlüsseln und Ausgabe in Console
-//        byte[] encryptText = AesCipher.doFinal(byteText);
-//        System.out.println("Verschlüsselter Text:");
-//        System.out.write(encryptText);
-//
-////      Milestone
-//        //hier liest er den verschlüsselten Text aus, den er entschlüsselt
-//        byte[] cipherText = encryptText;
-//
-//        //Entschlüsseln
-//        AesCipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-//        byte[] bytePlainText = AesCipher.doFinal(cipherText);
-//        
-//        //Ausgabe in Console
-//        System.out.println("\n \n Entschlüsselter Text:");
-//        System.out.write(bytePlainText);
-//    }
 
     
     public void encrypt() throws Exception {
