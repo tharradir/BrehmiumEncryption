1cf844b1368e5d949a69e884f6d67ba59e8e1d23
Tobias Möller <t.moeller95@hotmail.de>
2015-04-13 16:12:23 +0200

    Beispiel für RSA Ver- und ENtschlüsselung hinzugefügt

--- EncryptionExamples/src/de/eah_jena/source/AsymmetricKeyEncription.java
+++ EncryptionExamples/src/de/eah_jena/source/AsymmetricKeyEncription.java
@@ -1,5 +1,38 @@
 package de.eah_jena.source;
 
-public class AsymmetricKeyEncription {
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.Security;
+//import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+
+import javax.crypto.Cipher;
+
+@SuppressWarnings("unused")
+public class AsymmetricKeyEncription {
+	
+	public static void main(String[] args) throws Exception {
+	
+//	 Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+	    byte[] input = "aa".getBytes();
+	    Cipher cipher = Cipher.getInstance("RSA/None/NoPadding", "BC");
+	    SecureRandom random = new SecureRandom();
+	    KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", "BouncyCastle");
+
+	    generator.initialize(256, random);
+	    KeyPair pair = generator.generateKeyPair();
+	    Key pubKey = pair.getPublic();
+	    Key privKey = pair.getPrivate();
+
+	    cipher.init(Cipher.ENCRYPT_MODE, pubKey, random);
+
+	    byte[] cipherText = cipher.doFinal(input);
+	    System.out.println("cipher: " + new String(cipherText));
+	    cipher.init(Cipher.DECRYPT_MODE, privKey);
+	    byte[] plainText = cipher.doFinal(cipherText);
+	    System.out.println("plain : " + new String(plainText));
+	  }
 }
