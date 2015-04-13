f03d363400bf9fb75a93a8967dc863f99b5d97c8
Tobias Möller <t.moeller95@hotmail.de>
2015-04-13 10:12:55 +0200

    Kommentare einfügen und Formatieren

--- EncryptionExamples/src/de/eah_jena/source/GenerateHash.java
+++ EncryptionExamples/src/de/eah_jena/source/GenerateHash.java
@@ -1,40 +1,28 @@
 package de.eah_jena.source;
+
 import java.io.*;
 
 public class GenerateHash {
 
-public static void main(String args[]){
-	
-	System.out.println("Bitte Text angeben, von dem Hashwert erzeugt werden soll:");
-//	Konsolenobjekt für Eingabe initialisieren
-	BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-    String Str = null;
-//  Ausgangstext einlesen  
-	try {
-		Str = new String(console.readLine());
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-//  erzeugten Haswert ausgeben
-    System.out.println("Hashwert für eingegebenen String:" + Str.hashCode() );
- }
+	public void generateHash() {
 
-public void generateHash() {
-	
-	System.out.println("Bitte Text angeben, von dem Hashwert erzeugt werden soll:");
-//	Konsolenobjekt für Eingabe initialisieren
-	BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-    String Str = null;
-//  Ausgangstext einlesen  
-	try {
-		Str = new String(console.readLine());
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		// Konsolenobjekt für Eingabe initialisieren
+		BufferedReader console = new BufferedReader(new InputStreamReader(
+				System.in));
+		String Str = null;
+
+		// Ausgangstext einlesen
+		System.out
+				.println("Bitte Text angeben, von dem Hashwert erzeugt werden soll:");
+		try {
+			Str = new String(console.readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// erzeugten Haswert ausgeben
+		System.out.println("\nHashwert für eingegebenen Text: "
+				+ Str.hashCode());
 	}
-//  erzeugten Haswert ausgeben
-    System.out.println("\nHashwert für eingegebenen Text: " + Str.hashCode() );
-}
 
 }
--- EncryptionExamples/src/de/eah_jena/source/SymmetricKeyEncription.java
+++ EncryptionExamples/src/de/eah_jena/source/SymmetricKeyEncription.java
@@ -1,7 +1,6 @@
 package de.eah_jena.source;
 
 import javax.crypto.*;
-//zur Schlüsselgenerierung 
 import javax.crypto.spec.SecretKeySpec;
 
 import java.io.BufferedReader;
@@ -25,55 +24,58 @@ public class SymmetricKeyEncription {
 	private MessageDigest sha;
 
 	public SymmetricKeyEncription() throws Exception {
-		// TODO Auto-generated constructor stub
-		System.out.println("Bitte Schlüssel/Passwort angeben:");
+
+
 		// Konsolenobjekt für Eingabe initialisieren
 		BufferedReader console = new BufferedReader(new InputStreamReader(
 				System.in));
-		// String keyStr = null;
-		// Ausgangstext einlesen
+
+		// Schlüsselwort abfragen
+		System.out.println("Bitte Schlüssel/Passwort angeben:");
 		try {
 			keyStr = new String(console.readLine());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		// Ausgangstext einlesen
 		System.out.println("\nBitte zu verschlüsselnden Text eingeben: ");
 		try {
 			textStr = new String(console.readLine());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		// Schlüssel-String in Byte-Format umwandeln
 		key = (keyStr).getBytes("UTF-8");
+
+		// Text-String in Byte-Format umwandeln
 		byteText = textStr.getBytes("UTF-8");
+
+		// Objekt AesCipher Verschlüsselungsverfahren zuweisen
 		AesCipher = Cipher.getInstance("AES");
+
 		// aus dem Array einen Hash-Wert mit MD5 oder SHA erzeugen
 		sha = MessageDigest.getInstance("MD5");
 		key = sha.digest(key);
+
 		// nur die ersten 128 bit nutzen; da AES auf eine feste Größe beschränkt
-		// ist
 		key = Arrays.copyOf(key, 16);
+
+		// geheimen Schlüssel aus eingegebenem Schlüsseltext erzeugen
 		secretKeySpec = new SecretKeySpec(key, "AES");
 	}
 
 	public void encrypt() throws Exception {
 
-		// byte-Array erzeugen
-		// aus dem Array einen Hash-Wert erzeugen mit MD5 oder SHA
-		// MessageDigest sha = MessageDigest.getInstance("MD5");
-		// der fertige Schluessel
-		// Ausgangstext einlesen
-		// zu verschlüsselnder Text
-		// byte[] byteText = textStr.getBytes();
 		// Verschlüsselungsmodus der Cipher-Klasse initialisieren
 		AesCipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
 
-		// Verschlüsseln und Ausgabe in Console
+		// Verschlüsseln
 		encryptText = AesCipher.doFinal(byteText);
-		System.out.println("\nVerschlüsselter Text:");
 
+		// Ausgabe in Console
+		System.out.println("\nVerschlüsselter Text:");
 		System.out.write(encryptText);
 
 	}
@@ -94,31 +96,34 @@ public class SymmetricKeyEncription {
 
 	public void decryptCustomEncryptedText() throws Exception {
 
+		// Konsolenobjekt erzeugen
 		BufferedReader console = new BufferedReader(new InputStreamReader(
 				System.in));
+
+		// verschlüsselten Text abfragen
 		System.out.println("\n \nVerschlüsselter Text:");
-		// String cipherTextCustom = new String(console.readLine());
 		String inputText = new String(console.readLine());
 
-		// hier liest er den verschlüsselten Text aus, den er entschlüsselt
-
+		// verschlüsselten Text in Byte-Format umwandeln
 		byte[] cipherTextCustom1 = (inputText).getBytes();
 
+		// Schlüssel/Passwort der Verschlüsselung abfragen
 		System.out.println("\nBitte Schlüssel/Passwort angeben:");
-
 		String inputKey = new String(console.readLine());
 
+		// Schlüssel in Byte-Format umwandeln
 		byte[] keyCustom = (inputKey).getBytes("UTF-8");
+
 		// aus dem Array einen Hash-Wert erzeugen mit MD5 oder SHA
 		keyCustom = sha.digest(keyCustom);
+
 		// nur die ersten 128 bit nutzen; da AES auf eine feste Größe beschränkt
-		// ist
 		keyCustom = Arrays.copyOf(keyCustom, 16);
-		// der fertige Schluessel
+
+		// den geheimen Schluessel erzeugen
 		SecretKeySpec secretKeySpecCustom = new SecretKeySpec(keyCustom, "AES");
 
-		// System.out.println(keyCustom.length);
-
+		// "Entschlüsselungsmodus" initialisieren und entschlüsseln
 		AesCipher.init(Cipher.DECRYPT_MODE, secretKeySpecCustom);
 		byte[] bytePlainTextCustom = AesCipher.doFinal(cipherTextCustom1);
 
