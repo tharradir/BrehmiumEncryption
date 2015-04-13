4aff889d7011639cc813b7dcd01fb7ee6bf5043d
Tobias Möller <t.moeller95@hotmail.de>
2015-04-13 09:20:16 +0200

    Verbesserungen

--- EncryptionExamples/src/de/eah_jena/main/Main.java
+++ EncryptionExamples/src/de/eah_jena/main/Main.java
@@ -7,17 +7,16 @@ import de.eah_jena.source.SymmetricKeyEncription;
 
 public class Main {
 	
-//	private byte[] buf = s«nÏj|HØÊT†ÛaEw¿;
+
+	
+	
 public static void main(String[] args) throws Exception {
-//	GenerateHash Hash = new GenerateHash();
-//	Hash.generateHash();
 	
 	SymmetricKeyEncription Symm = new SymmetricKeyEncription();
 	Symm.encrypt();
 	Symm.decrypt();
 	Symm.decryptCustomEncryptedText();
 	
-//	ByteArrayInputStream input = new ByteArrayInputStream(buf);
 	
 }
 }
